cd9cd8eb9d8b23194aafa19646630df4d2437752
Chirag Dhawan <cdhawan7@gmail.com>
2026-08-27 17:16:11 -0700

    Bootstrap CareLoop .NET project structure

---
+++ tests/CareLoop.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CareLoop.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/CareLoop.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace CareLoop.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
